fe7421095caeda4584f5bd1f91985d078e8962d9
mrzogy <magbolmrzogy@gmail.com>
2023-05-21 17:44:53 +0300

    Initial Commit

---
+++ BMI/BMIApp.swift
@@ -0,0 +1,17 @@
+//
+//  BMIApp.swift
+//  BMI
+//
+//  Created by 3bood on 01/11/1444 AH.
+//
+
+import SwiftUI
+
+@main
+struct BMIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
